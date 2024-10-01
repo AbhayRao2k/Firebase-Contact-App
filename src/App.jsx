f7ed1481f87011b7bbae8ca926a364f0096025a2
Abhay Rao <abhayrao2k@gmail.com>
2024-10-01 12:08:32 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import { FiSearch } from "react-icons/fi";
+import Navbar from "./components/Navbar";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./config/firebase";
+import { HiOutlineUserCircle } from "react-icons/hi";
+import { IoMdTrash } from "react-icons/io";
+import { RiEditCircleLine } from "react-icons/ri";
+import ContactCard from "./components/ContactCard";
+import Modal from "./components/Modal";
+
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [isOpen, setOpen] = useState(false);
+
+  const onOpen = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        const contactsRef = collection(db, "contacts");
+        const contactsSnapshot = await getDocs(contactsRef);
+        const contactLists = contactsSnapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+        setContacts(contactLists);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getContacts();
+  }, []);
+
+  return (
+    <>
+      <div className="max-w-[370px] mx-auto px-4">
+        <Navbar />
+        <div className="flex gap-2">
+          <div className="flex relative items-center flex-grow">
+            <FiSearch className="ml-1 text-white text-3xl absolute" />
+            <input
+              type="text"
+              className="h-10 flex-grow rounded-md border border-white bg-transparent text-white pl-9"
+            />
+          </div>
+          <AiFillPlusCircle onClick={onOpen} className="cursor-pointer text-5xl text-white" />
+        </div>
+        <div className="mt-4 flex flex-col gap-3">
+          {contacts.map((contact) => (
+            <ContactCard key={contact.id} contact={contact} />
+          ))}
+        </div>
+      </div>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        HI
+      </Modal>
+    </>
+  );
+};
+
+export default App;
