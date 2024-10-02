f87b97467611255c926b86a233f0eb8b774f7b45
Abhay Rao <abhayrao2k@gmail.com>
2024-10-02 17:20:44 +0530

    completed project

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,97 @@
-import { FiSearch } from "react-icons/fi";
 import Navbar from "./components/Navbar";
+import { FiSearch } from "react-icons/fi";
 import { AiFillPlusCircle } from "react-icons/ai";
 import { useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, onSnapshot } from "firebase/firestore";
 import { db } from "./config/firebase";
-import { HiOutlineUserCircle } from "react-icons/hi";
-import { IoMdTrash } from "react-icons/io";
-import { RiEditCircleLine } from "react-icons/ri";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import ContactCard from "./components/ContactCard";
-import Modal from "./components/Modal";
-
+import AddAndUpdateContact from "./components/AddAndUpdateContact";
+import useDisclouse from "./hooks/useDisclouse";
+import NotFoundContact from "./components/NotFoundContact";
 const App = () => {
   const [contacts, setContacts] = useState([]);
-  const [isOpen, setOpen] = useState(false);
 
-  const onOpen = () => {
-    setOpen(true);
-  };
-
-  const onClose = () => {
-    setOpen(false);
-  };
+  const { isOpen, onClose, onOpen } = useDisclouse();
 
   useEffect(() => {
     const getContacts = async () => {
       try {
         const contactsRef = collection(db, "contacts");
-        const contactsSnapshot = await getDocs(contactsRef);
-        const contactLists = contactsSnapshot.docs.map((doc) => {
-          return {
-            id: doc.id,
-            ...doc.data(),
-          };
+
+        onSnapshot(contactsRef, (snapshot) => {
+          const contactLists = snapshot.docs.map((doc) => {
+            return {
+              id: doc.id,
+              ...doc.data(),
+            };
+          });
+          setContacts(contactLists);
+          return contactLists;
         });
-        setContacts(contactLists);
       } catch (error) {
         console.log(error);
       }
     };
+
     getContacts();
   }, []);
 
+  const filterContacts = (e) => {
+    const value = e.target.value;
+
+    const contactsRef = collection(db, "contacts");
+
+    onSnapshot(contactsRef, (snapshot) => {
+      const contactLists = snapshot.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
+      });
+
+      const filteredContacts = contactLists.filter((contact) =>
+        contact.name.toLowerCase().includes(value.toLowerCase())
+      );
+
+      setContacts(filteredContacts);
+
+      return filteredContacts;
+    });
+  };
+
   return (
     <>
-      <div className="max-w-[370px] mx-auto px-4">
+      <div className="mx-auto max-w-[370px] px-4">
         <Navbar />
         <div className="flex gap-2">
-          <div className="flex relative items-center flex-grow">
-            <FiSearch className="ml-1 text-white text-3xl absolute" />
+          <div className="relative flex flex-grow items-center">
+            <FiSearch className="absolute ml-1 text-3xl text-white" />
             <input
+              onChange={filterContacts}
               type="text"
-              className="h-10 flex-grow rounded-md border border-white bg-transparent text-white pl-9"
+              className=" h-10 flex-grow rounded-md border border-white bg-transparent pl-9 text-white"
             />
           </div>
-          <AiFillPlusCircle onClick={onOpen} className="cursor-pointer text-5xl text-white" />
+
+          <AiFillPlusCircle
+            onClick={onOpen}
+            className="cursor-pointer text-5xl text-white"
+          />
         </div>
         <div className="mt-4 flex flex-col gap-3">
-          {contacts.map((contact) => (
-            <ContactCard key={contact.id} contact={contact} />
-          ))}
+          {contacts.length <= 0 ? (
+            <NotFoundContact />
+          ) : (
+            contacts.map((contact) => (
+              <ContactCard key={contact.id} contact={contact} />
+            ))
+          )}
         </div>
       </div>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        HI
-      </Modal>
+      <ToastContainer position="bottom-center" />
+      <AddAndUpdateContact onClose={onClose} isOpen={isOpen} />
     </>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 const Navbar = () => {
   return (
-    <div className="m-4 flex h-[60px] bg-white rounded-lg items-center justify-center gap-2 text-xl font-medium">
-      <img src="/logos_firebase.svg" alt="logo" />
+    <div className="my-4 flex h-[60px] items-center justify-center gap-2 rounded-lg bg-white text-xl font-medium">
+      <img src="/firebase.svg" />
       <h1>Firebase Contact App</h1>
     </div>
   );
